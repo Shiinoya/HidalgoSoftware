3a9222370bca4505e098df51137a06813c211aad
Shiinoya <shiinoya94@gmail.com>
2024-11-28 11:14:53 -0700

    Install sass + new app.scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import '../app.scss';
+</script>
+
+<!--
+@component
+
+## component_name
+component_description
+
+### Features
+feature_description
+
+### Props
+- prop_name: prop_description
+-->
+
+<title></title>
+
+<div></div>
+
+<style lang="scss"></style>
