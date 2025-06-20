b454e87bbdf19facea861609e40fff86d5f149a4
Shiinoya <shiinoya94@gmail.com>
2025-06-19 21:11:47 -0700

    Implement adding new timers

--- src/routes/applications/timer/+page.svelte
+++ src/routes/applications/timer/+page.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
   import Timer from '$lib/components/smart/Timer.svelte';
+
+  let timers = [];
 </script>
 
 <title>Board Game Timer</title>
 
 <div class="view-wrapper">
+  
   <h1>Board Game Timer</h1>
-  <Timer />
+  <button onclick={() => (timers = [...timers, Timer])}>Add timer</button>
+
+  <div class="timers-wrapper">
+    {#each timers as timer}
+      <svelte:component this={timer} />
+    {/each}
+  </div>
 </div>
 
 <style lang="scss">
   div.view-wrapper {
-    // TODO : make this response
-    height: 500px;
+    // TODO : make this responsive
 
     display: flex;
     flex-direction: column;
@@ -21,6 +29,12 @@
     h1 {
       text-align: center;
       line-height: 1.2em;
+    }
+
+    div.timers-wrapper {
+      display: grid;
+      grid-template-columns: repeat(3, auto);
+      grid-template-rows: repeat(3, auto);
     }
   }
 </style>
