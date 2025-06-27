64982c7ed5ac22d4c3b966e3c9c455a84ea02b7d
Shiinoya <shiinoya94@gmail.com>
2025-06-27 11:29:18 -0700

    Create a player store that syncs with localStorage

---
+++ src/lib/stores/players.svelte.ts
@@ -0,0 +1,26 @@
+import { browser } from '$app/environment';
+import { setLocalStorage, getLocalStorage } from '$lib/utils/localStorage.svelte';
+
+type Player = {
+  name: string;
+  time: number;
+  icon: string;
+  color: string;
+};
+
+export const players = $state<Player[]>([]);
+
+// get & hydrate players
+if (browser) {
+  const saved: Player[] = getLocalStorage('players') ?? [];
+
+  if (saved) {
+    players.length = 0; // clear array but maintain reactivity across components
+    players.push(...saved);
+  }
+}
+
+// set players
+$effect(() => {
+  if (browser) setLocalStorage('players', players);
+});
