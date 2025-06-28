85f7617004633e7a28e7b840b63e537759c116c1
Shiinoya <shiinoya94@gmail.com>
2025-06-28 12:37:27 -0700

    New PlayerCard component

---
+++ src/lib/components/smart/PlayerCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts"></script>
+
+<!--
+@component
+
+## Player Card
+component_description
+
+### Features
+feature_description
+
+### Props
+- prop_name: prop_description
+-->
+
+<div class=component-wrapper>
+  
+</div>
+
+<style lang="scss"></style>
