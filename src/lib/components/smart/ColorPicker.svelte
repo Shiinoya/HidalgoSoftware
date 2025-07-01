5a58b305d9beb1fc91500f9dce3a52e70b98f1c6
Shiinoya <shiinoya94@gmail.com>
2025-07-01 11:27:32 -0700

    ColorPicker shows selected color

--- src/lib/components/smart/ColorPicker.svelte
+++ src/lib/components/smart/ColorPicker.svelte
@@ -1,34 +1,80 @@
 <script lang="ts">
   import ColorSelect from 'svelte-color-select';
 
-  let rgb = { r: 0.4, g: 0.2, b: 0.6 };
+  let showColorPicker = $state(false);
+
+  let rgb = $state({ r: 0.4, g: 0.2, b: 0.6 });
+
+  let currentColor = $state({
+    r: 255,
+    g: 255,
+    b: 255
+  });
 
   function formatRGB(value: number): number {
     return Math.round(value * 255);
   }
+
+  $effect(() => {
+    currentColor = {
+      r: formatRGB(rgb.r),
+      g: formatRGB(rgb.g),
+      b: formatRGB(rgb.b)
+    };
+  });
 </script>
 
 <!--
 @component
 
 ## Color Picker
-component_description
+Component for selecting colors.
 
 ### Features
-feature_description
+Allows user to select a color using a drag and drop. The selected color is displayed as the icon's color.
 
 ### Props
 - prop_name: prop_description
 -->
 
-<title></title>
+<div class="component-wrapper">
+  <button class="open-close" onclick={() => (showColorPicker = !showColorPicker)}>
+    <span
+      class="material-symbols-outlined"
+      style={`color: rgb(${currentColor.r}, ${currentColor.g}, ${currentColor.b})`}
+    >
+      palette
+    </span>
+  </button>
 
-<div>
-  <ColorSelect bind:rgb />
-
-  <div>
-    {formatRGB(rgb.r)} {formatRGB(rgb.g)} {formatRGB(rgb.b)}
+  <div class="color-picker" class:showColorPicker>
+    <ColorSelect bind:rgb />
   </div>
 </div>
 
-<style lang="scss"></style>
+<style lang="scss">
+  div.component-wrapper {
+    button.open-close {
+      background: none;
+      border: none;
+      cursor: pointer;
+
+      span {
+        font-size: 3rem;
+      }
+    }
+
+    div.color-picker {
+      display: none;
+
+      &.showColorPicker {
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        background: black;
+      }
+    }
+  }
+</style>
--- src/lib/components/smart/PlayerCard.svelte
+++ src/lib/components/smart/PlayerCard.svelte
@@ -24,21 +24,35 @@ feature_description
 -->
 
 <div class="component-wrapper">
+  <div class="name-color">
   <span>Player Name:<input value={player.name} /></span>
+  <ColorPicker />
+</div>
   <span>Player Time:<input value={player.time} type="number" /></span>
   <span>Player Icon:<input value={player.icon} /></span>
-  <span>Player Color:<input value={player.color} /></span>
-  <ColorPicker />
 </div>
 
 <style lang="scss">
   div.component-wrapper {
+    max-width: 400px;
+
+    padding: 2rem;
+
     display: flex;
     flex-direction: column;
     gap: 1rem;
 
+    // this color should match the color of the icon in ColorPicker
+    border: 1px solid rgba(198, 145, 247, 0.9);
+    border-radius: 20px;
+
     input {
       margin-left: 1rem;
+    }
+
+    .name-color {
+      display: flex;
+      gap: 2rem;
     }
   }
 </style>
